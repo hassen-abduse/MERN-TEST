d3f9038de7a927e70979879b38dea0554dd550fa
hassen-abduse <hassen.abduse@gmail.com>
2021-09-29 23:16:58 +0300

    added Employee model

---
+++ server/models/Employee.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const employeeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Employees = mongoose.model('Employee', employeeSchema)
+
+module.exports = Employees
