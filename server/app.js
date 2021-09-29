8271333db11a3ddbc91faa0b40898321f5d8df7e
hassen-abduse <hassen.abduse@gmail.com>
2021-09-30 00:32:38 +0300

    configured MONGODB_URI

--- server/app.js
+++ server/app.js
@@ -1,26 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express, { json, urlencoded, static } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose'
+import employeesRouter from './routes/employees'
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const MONGODB_URI = process.env.MONGODB_URI
+const connect = mongoose.connect(MONGODB_URI)
+
+connect.then((db) => {
+  console.log('Succesfully Connected to the DB Server.')
+}, (err) => console.log(err))
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/employees', employeesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +47,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
